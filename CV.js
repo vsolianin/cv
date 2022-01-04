b846a27fb904f8161fe3f2b846a2a617d09b4944
vsolianin <vsolianin@gmail.com>
2022-01-04 21:01:42 +0200

    cv commit

---
+++ CV.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+	var a;
+	a = 6;
+	console.dir(a, "a");
+
+	body = document.querySelector("body");
+	console.dir(body, "body");
+	// document.body.style.background = "red";
+
+	// setTimeout(function() {
+	// 	document.body.style.background = ""
+	// }, 2000);
+
+	console.log(location.href, "locationHref");
+
+}
